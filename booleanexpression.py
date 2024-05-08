ac1994503ce9b1dbea003240ed0275f66aa9c39e
diisharana <168515517+diisharana@users.noreply.github.com>
2024-05-08 06:41:18 +0000

    boolena expressions added

---
+++ booleanexpression.py
@@ -0,0 +1,33 @@
+#Learning Boolean Expressions in Python
+
+#Boolean exp- that results in True or False 
+print( 'I am inside booleanexpression.py')
+print (10 > 20)
+
+
+#comparison operators - results in True or False
+# >
+# <
+# >=
+# <=
+# ==
+# !=
+x = 10
+y = 20
+print (x > y)
+print (x < y)
+print (x != y)
+print (x == y)
+
+#logical operators - results in True or False
+# and | or | not
+# and results in True if both expressions are True
+# or results in True if any one expression is True
+# not is opposite, True if expression is False and vice versa
+
+x = 10
+y = 20
+z = 30
+print (x < y and z > y)
+print (x < y or z < y)
+print(not(x > y))
